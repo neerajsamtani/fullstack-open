eb23de242e870e6ef38d32c5599132dd97122d78
Neeraj Samtani <neerajjsamtani@gmail.com>
2020-06-24 03:07:40 +0400

    Completed 2d lesson

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,16 +8,29 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
   }
   
   useEffect(hook, [])
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      alert(`the note ${note.content} was already deleted from the server`)
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -25,11 +38,14 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
@@ -49,8 +65,12 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note, i) =>
+          <Note 
+          key={i} 
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
+           />
         )}
       </ul>
       <form onSubmit={addNote}>
--- part2/notes/src/components/Note.js
+++ part2/notes/src/components/Note.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Note = ({ note }) => (
-    <li >
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+  return (
+    < li >
       {note.content}
-    </li>
+      <button onClick={toggleImportance}>{label}</button>
+    </li >
   )
+}
 
 export default Note
